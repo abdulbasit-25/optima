c0290055b40b62e715815355d2977a3dc0f88018
abdulbasit-025 <abdulbasit.alpha25@gmail.com>
2026-09-12 00:42:56 +0500

    Update vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
+
+export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
+  plugins: [
+    tanstackStart({
+      server: { entry: "server" },
+    }),
+    nitro(),
+    react(),
+    tailwindcss(),
+  ],
+});
